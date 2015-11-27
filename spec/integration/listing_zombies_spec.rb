a5dce0d0e65f049d28bae4748357427cec4cc91f
Carolina Borim <cborim@thoughtworks.com>
2015-11-27 17:12:31 -0200

    first test passing

--- spec/integration/listing_zombies_spec.rb
+++ spec/integration/listing_zombies_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 describe 'zombies list', type: :request do
-
-setup { host! 'api.example.com' }
+    before :example do
+      host! 'api.example.com'
+    end
 
   it 'returns list of all zombies' do
-    Zombie.create!(name: 'Ash', weapon: 'axe')
-
     get '/zombies'
 
     expect(response.status).to eq 200
