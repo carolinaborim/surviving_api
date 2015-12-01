85a40110a799f9b226dd9e3f3f946ad422a7c1f6
Carolina Borim <cborim@thoughtworks.com>
2015-12-01 18:20:58 -0200

    making test pass

--- app/controllers/api/zombies_controller.rb
+++ app/controllers/api/zombies_controller.rb
@@ -1,7 +1,11 @@
 module API
   class ZombiesController < ApplicationController
     def index
-      zombies = Zombie.all
+      if weapon = params[:weapon]
+        zombies = Zombie.where(weapon: weapon)
+      else
+        zombies = Zombie.all
+      end
       render json: zombies, status: 200
     end
 
